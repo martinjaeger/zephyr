90faf3fd88194f7b73820cf7352226d7f011d90f
Martin Jäger <martin@libre.solar>
2022-07-18 10:45:34 +0200

    samples: lorawan: fuota: add basic sample application
    Add basic sample application that starts the LoRaWAN FUOTA service.
    
    Signed-off-by: Martin Jäger <martin@libre.solar>

---
+++ samples/subsys/lorawan/fuota/src/main.c
@@ -0,0 +1,87 @@
+/*
+ * LoRaWAN FUOTA sample application
+ *
+ * Copyright (c) 2022 Martin Jäger
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/device.h>
+#include <zephyr/lorawan/lorawan.h>
+#include <zephyr/zephyr.h>
+
+#define DEFAULT_RADIO_NODE DT_ALIAS(lora0)
+BUILD_ASSERT(DT_NODE_HAS_STATUS(DEFAULT_RADIO_NODE, okay), "No LoRa radio specified in DT");
+#define DEFAULT_RADIO DT_LABEL(DEFAULT_RADIO_NODE)
+
+/* Customize based on device configuration */
+#define LORAWAN_DEV_EUI		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+#define LORAWAN_JOIN_EUI	{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+#define LORAWAN_APP_KEY		{ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,\
+				  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(lorawan_fuota_sample);
+
+static void downlink_info(uint8_t port, bool data_pending, int16_t rssi, int8_t snr,
+			  uint8_t len, const uint8_t *data)
+{
+	LOG_INF("Port %d, Pending %d, RSSI %ddB, SNR %ddBm", port, data_pending, rssi, snr);
+	if (data) {
+		LOG_HEXDUMP_INF(data, len, "Payload: ");
+	}
+}
+
+static void datarate_changed(enum lorawan_datarate dr)
+{
+	uint8_t unused, max_size;
+
+	lorawan_get_payload_sizes(&unused, &max_size);
+	LOG_INF("New Datarate: DR_%d, Max Payload %d", dr, max_size);
+}
+
+void main(void)
+{
+	const struct device *lora_dev;
+	struct lorawan_join_config join_cfg;
+	uint8_t dev_eui[] = LORAWAN_DEV_EUI;
+	uint8_t join_eui[] = LORAWAN_JOIN_EUI;
+	uint8_t app_key[] = LORAWAN_APP_KEY;
+	int ret;
+
+	struct lorawan_downlink_cb downlink_cb = {
+		.port = LW_RECV_PORT_ANY,
+		.cb = downlink_info
+	};
+
+	lora_dev = device_get_binding(DEFAULT_RADIO);
+	if (!lora_dev) {
+		LOG_ERR("%s Device not found", DEFAULT_RADIO);
+		return;
+	}
+
+	ret = lorawan_start();
+	if (ret < 0) {
+		LOG_ERR("lorawan_start failed: %d", ret);
+		return;
+	}
+
+	lorawan_register_downlink_callback(&downlink_cb);
+	lorawan_register_dr_changed_callback(datarate_changed);
+
+	join_cfg.mode = LORAWAN_ACT_OTAA;
+	join_cfg.dev_eui = dev_eui;
+	join_cfg.otaa.join_eui = join_eui;
+	join_cfg.otaa.app_key = app_key;
+	join_cfg.otaa.nwk_key = app_key;
+
+	LOG_INF("Joining network over OTAA");
+	ret = lorawan_join(&join_cfg);
+	if (ret < 0) {
+		LOG_ERR("lorawan_join_network failed: %d", ret);
+		return;
+	}
+
+	lorawan_start_fuota_service();
+}
